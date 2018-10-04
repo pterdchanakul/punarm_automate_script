7fa88f6819b9ba49c5a44a4d4454a1039507cc0e
Pannavat Terdchanakul <Pannavat.T@gmail.com>
2018-10-04 09:14:11 +0700

    Update hyperledger_fabric_prereq_version2.sh
    change code

--- fabric/hyperledger_fabric_prereq_version2.sh
+++ fabric/hyperledger_fabric_prereq_version2.sh
@@ -109,27 +109,3 @@ sudo usermod -aG docker $USER
 echo ''
 echo "Please logout then login before continuing."
 
-sudo apt-get update
-
-echo "Install Golang"
-wget -c https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
-tar -C ~ -xvzf go1.10.3.linux-amd64.tar.gz
-
-echo "Export Golang Path"
-sudo echo "export GOPATH=$HOME/go" >> .bashrc
-echo "export PATH=$HOME/go/bin:$PATH" >> .bashrc
-source .bashrc
-
-
-echo "Install Hyperledger-Fabric Image and CA and Docker"
-mkdir Program
-cd Program
-curl -sSL http://bit.ly/2ysbOFE | bash -s 1.2.0 1.2.0 0.4.10
-cd fabric-samples
-echo -n "export PATH=$PWD/bin:$PATH" >> .bashrc
-source .bashrc
-
-echo "Finished Install Hyperledger Fabric"
-go version
-cryptogen version
-
